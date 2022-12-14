b4aa95403d96efba258e62471df692936c53af55
adkshz <120451483+adkshz@users.noreply.github.com>
2022-12-14 22:38:35 +0900

    Push
    TestCommit

---
+++ 4_2_text.py
@@ -0,0 +1,14 @@
+print('Hello world')
+print("Hello world")
+print('''
+Hello
+world
+''')
+
+print ("'1'+'1'", '1'+'1')
+print ('Hello world'*10)
+print("len('hello world'*1000)", len('hello world'*10))
+print("'Hello world'.replace('world', 'universe')", 'Hello world'. 
+replace('world', 'universe'))
+
+print (1+1, 1+1)
---
+++ 4_3_list.py
@@ -0,0 +1,15 @@
+students = ["egoing", "sori" , "maru"]
+grades = [2,1,4]
+print("students[1]", students[1])
+print("students[0]", students[0])
+print("students[2]", students[2])
+print("len(students)", len(students))
+print("min(grades)", min(grades))
+print("min(grades)", max(grades))
+print("sum(grades)", sum(grades))
+
+import statistics
+print("statistics.mean(grades)", statistics.mean(grades))
+
+import random
+print("random.choice(students)", random.choice(students))
