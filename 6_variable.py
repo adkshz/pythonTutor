bcfa8a3705ac45e55e3f36aa935b259d488e6625
adkshz <120451483+adkshz@users.noreply.github.com>
2022-12-15 23:02:50 +0900

    Create 6_variable.py

---
+++ 6_variable.py
@@ -0,0 +1,9 @@
+name = 'egoing'
+massage = 'hi, '+name+' .... bye, '+name+'.'
+print(massage)
+
+print('hello World!')
+
+name2 = 'Ae_la'
+massage2 = 'hi, '+name2+' .... bye, '+name2+'.'
+print(massage2)
